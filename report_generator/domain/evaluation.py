1543d0a9e49463161e66258c61e3cc76117cbb11
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2020-07-09 10:29:46 -0400

    Adds Evaluation Data Structure

--- report_generator/domain/evaluation.py
+++ report_generator/domain/evaluation.py
@@ -1,4 +1,16 @@
 
 
 class Evaluation:
-    pass
+
+    def __init__(self, professor, course, student_count, question_count, score, maximum):
+        self.professor = professor
+        self.course = course
+        self.student_count = student_count
+        self.question_count = question_count
+        self.score = score
+        self.maximum = maximum
+
+    @property
+    def average(self):
+        # Formula: number_of_points_obtained / question_count * student_count
+        return 0
